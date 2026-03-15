49e4a96cd9fa5569161a2acd119fdb356486077e
dropboy27 <pyanzinmihail@gmail.com>
2026-03-15 22:31:04 +0300

    финал

--- src/main.py
+++ src/main.py
@@ -4,16 +4,18 @@ from src.TaskSources.api_task import ApiTaskSource
 from src.recieve_tasks import receive
 from src.exceptions.file_task_source_exceptions import TaskSourceError
 from src.exceptions.task_exceptions import TaskError
+from src.classes.task_queue import TaskQueue
 
 
 def main() -> None:
-    tasks = []
+    tasks = TaskQueue()
     while True:
         print("\n1 - Прочитать задачу из файла\n"
               "2 - Сгенерировать задачи\n"
               "3 - API заглушка\n"
               "4 - Увидеть задачи\n"
-              "5 - Выход\n")
+              "5 - фильтры\n"
+              "6 - Выход\n")
         try:
             variant = int(input("Выберите пункт: "))
         except ValueError:
@@ -24,17 +26,20 @@ def main() -> None:
             if variant == 1:
                 filename = input("Укажите файл: ")
                 src = FileTaskSource(filename)
-                tasks.extend(receive(src))
+                for t in receive(src):
+                    tasks.add_task(t)
 
             elif variant == 2:
                 num = int(input("Укажите количество задач: "))
                 src = TaskGenerator(num)
-                tasks.extend(receive(src))
+                for t in receive(src):
+                    tasks.add_task(t)
 
             elif variant == 3:
                 num = int(input("Укажите количество задач для API: "))
                 src = ApiTaskSource(tasks_count=num, simulate_delay=True)
-                tasks.extend(receive(src))
+                for t in receive(src):
+                    tasks.add_task(t)
 
             elif variant == 4:
                 if not tasks:
@@ -52,11 +57,46 @@ def main() -> None:
                         print(t)
                 elif choice == 2:
                     for t in tasks:
+                        print(t.short_desc)
+                else:
+                    print("Неверный выбор")
+            elif variant == 5:
+                print("choose filter:\n"
+                      "1 - filter by priority\n"
+                      "2 - filter by status\n"
+                      "3 - filter by id\n")
+                choice = int(input())
+                if choice == 1:
+                    print("choose priority from 1 to 5")
+                    priority = int(input())
+                    if priority > 5 or priority < 1:
+                        raise ValueError("Incorrect priority")
+                    print(
+                        f"1 - lower than {priority}\n 2 - higher than {priority}\n 3 - equal to {priority}")
+                    condition = int(input())
+                    for t in tasks.filter_by_priority(priority, condition):
+                        print(t)
+                elif choice == 2:
+                    print("choose status from ('новая', 'в работе', 'завершена')")
+                    status = input()
+                    if status not in ('новая', 'в работе', 'завершена'):
+                        raise ValueError("Incorrect status")
+                    print(
+                        f"1 - show all tasks with {status}\n 2 - show all tasks without {status}")
+                    condition = int(input())
+                    for t in tasks.filter_by_status(status, condition):
+                        print(t)
+                elif choice == 3:
+                    print("choose id")
+                    id = int(input())
+                    print(
+                        f"1 - lower than {id}\n 2 - higher than {id}\n 3 - equal to {id}")
+                    condition = int(input())
+                    for t in tasks.filter_by_id(id, condition):
                         print(t)
                 else:
                     print("Неверный выбор")
-
-            elif variant == 5:
+            elif variant == 6:
                 break
 
             else:
